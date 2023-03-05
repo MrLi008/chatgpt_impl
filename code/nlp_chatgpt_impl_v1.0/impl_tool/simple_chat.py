7f41b1f509ab4ba7fe712e482a3bcd6743ace6c9
mrli008 <952934650@qq.com>
2023-03-05 08:17:04 +0800

    变更

--- code/nlp_chatgpt_impl_v1.0/impl_tool/simple_chat.py
+++ code/nlp_chatgpt_impl_v1.0/impl_tool/simple_chat.py
@@ -4,9 +4,10 @@
 @date 2023.03.05
 @description 简单聊天测试
 """
-
+import os
 import openai
 
-openai.api_key = 'sk-...'
+openai.api_key = os.getenv('chatgpt_key')
+print(openai.api_key)
 completion = openai.Completion(model='gpt-3.5-turbo', prompt='Hi')
 print(completion.choices[0].text)
